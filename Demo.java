36bf58a4e05bfdc464b8cd72032a881eb8c73c7a
akash <akash300383@gmail.com>
2022-06-22 20:54:16 +0530

    done

--- Demo.java
+++ Demo.java
@@ -2,5 +2,6 @@ class Demo {
 	public static void main(String args[]) {
 		System.out.println("This is simple java program running this jenkin");
 		System.out.println("This application running for batch8");
+		System.out.println("This application run once again");
 	}
 }
